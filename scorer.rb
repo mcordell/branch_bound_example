102452cef74085c01b58fb931f5cb7d7d88bed71
Michael Cordell <surpher@gmail.com>
2013-05-18 00:34:19 -0700

    ruby style guide refactor

--- scorer.rb
+++ scorer.rb
@@ -1,48 +1,48 @@
-
+# encoding: utf-8
 class Scorer
 
-  def initialize(requirements,main_requirement_label)
-    @requirements=requirements
-    @main_requirement_label=main_requirement_label
+  def initialize(requirements, main_requirement_label)
+    @requirements = requirements
+    @main_requirement_label = main_requirement_label
   end
 
-  #Get the summed values for each requirement within supplied widget array.
-  #Returns a hash where the key is the requirement and the sum is the value.
+  # Get the summed values for each requirement within supplied widget array.
+  # Returns a hash where the key is the requirement and the sum is the value.
   # @param [array] widget_array
   def get_values(widget_array)
-    values_hash={}
-    @requirements.each { |req,value| values_hash[req]= 0 }
+    values_hash = {}
+    @requirements.each { |req, value| values_hash[req] = 0 }
     widget_array.each do |widget|
-      @requirements.each do |requirement,value|
-        values_hash[requirement]+=widget.send(requirement)
+      @requirements.each do |requirement, value|
+        values_hash[requirement] += widget.send(requirement)
       end
     end
     return values_hash
   end
 
-  #Get the score of the array. Each requirement is subtracted current
-  #value, so scores less than or equal to 0 are filled requirements. Return a hash
-  #where key is the requirement and value is the score.
+  # Get the score of the array. Each requirement is subtracted current
+  # value, so scores less than or equal to 0 are filled requirements. Return
+  # a hash where key is the requirement and value is the score.
   def get_score_hash(widget_array)
-    values=get_values(widget_array)
-    score_hash={}
-    @requirements.each { |requirement,value| score_hash[requirement]= value - values[requirement] }
+    values = get_values(widget_array)
+    score_hash = {}
+    @requirements.each { |req, value| score_hash[req] = value - values[req] }
     return score_hash
   end
 
   def get_total_score(score_hash)
-      negatives=0
-      positives=0
-      @requirements.each do |requirement,value|
+      negatives = 0
+      positives = 0
+      @requirements.each do |requirement, value|
         if requirement != @main_requirement_label
-        value=score_hash[requirement]
-        value <= 0 ? negatives+=value : positives+=value
-          end
+          value = score_hash[requirement]
+          value <= 0 ? negatives += value : positives += value
+        end
       end
-      positives == 0 ? score=negatives : score=positives
+      positives == 0 ? score = negatives : score = positives
   end
 
-  def get_better_score(first,second)
+  def get_better_score(first, second)
     if first.nil?
       return -1
     elsif second.nil?
@@ -51,7 +51,7 @@ class Scorer
       return 0
     elsif second > 0 and second > first
       return 1
-    elsif second <= 0 and first <=0 and first > second
+    elsif second <= 0 and first <= 0 and first > second
       return 1
     else
       return -1
--- widget.rb
+++ widget.rb
@@ -1,12 +1,13 @@
+#encoding: utf-8
 class Widget
-  attr_accessor :name,:size, :power_a, :power_b, :power_c
+  attr_accessor :name, :size, :power_a, :power_b, :power_c
 
-  def initialize(name,size,power_a,power_b,power_c)
-    @name=name
-    @size=size
-    @power_a=power_a
-    @power_b=power_b
-    @power_c=power_c
+  def initialize(name, size, power_a, power_b, power_c)
+    @name = name
+    @size = size
+    @power_a = power_a
+    @power_b = power_b
+    @power_c = power_c
   end
 
   def to_s
